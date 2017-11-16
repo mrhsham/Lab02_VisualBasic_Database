6121dac35f5a871bd67801c1e8e69718e62d66b3
pkmitl205 <58030205@kmitl.ac.th>
2017-11-16 23:14:12 +0700

    ผลการทดลองที่ 3

--- StudentDatabase/StudentDatabase/Form1.vb
+++ StudentDatabase/StudentDatabase/Form1.vb
@@ -1,4 +1,23 @@
-﻿Public Class FrmStudentDB
+﻿Imports System.Data.Odbc
+
+Public Class FrmStudentDB
+    Enum commandStatusEnum
+        None = 0
+        Create = 1
+        Edit = 2
+    End Enum
+
+    Dim con As OdbcConnection
+    Dim myCommand As commandStatusEnum = commandStatusEnum.None
+
+    Private Function CreateConnection() As Boolean
+        Dim rslRtn As Boolean = True
+        con = New OdbcConnection()
+            Dim conStr As String = "Driver={Microsoft Access Driver (*.mdb)}; DBQ=TestDB.mdb;UID=;PWD=;"
+            con.ConnectionString = conStr
+        con.Open()
+        Return rslRtn
+    End Function
 
     Private Sub addCombo()
         With cbxTitle
@@ -12,5 +31,9 @@
 
     Private Sub FrmStudentDB_Load(sender As Object, e As EventArgs) Handles MyBase.Load
         addCombo()
+
+        If CreateConnection() = False Then
+            End
+        End If
     End Sub
 End Class
