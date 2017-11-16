3bea8944874a4f661aa1e8325f7edc85871bd4a7
pkmitl205 <58030205@kmitl.ac.th>
2017-11-16 23:02:39 +0700

    ผลการทดลองที่ 2

--- StudentDatabase/StudentDatabase/Form1.vb
+++ StudentDatabase/StudentDatabase/Form1.vb
@@ -1,5 +1,16 @@
 ﻿Public Class FrmStudentDB
 
+    Private Sub addCombo()
+        With cbxTitle
+            .Items.Clear()
+            .Items.Add("นาย")
+            .Items.Add("นาง")
+            .Items.Add("นางสาว")
+            .SelectedIndex = 0
+        End With
+    End Sub
 
-
+    Private Sub FrmStudentDB_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        addCombo()
+    End Sub
 End Class
